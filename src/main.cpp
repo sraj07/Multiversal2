f3db531ae18a45ddd2aa15b0cb1bc95604905a16
Sudev Raj <sudevrajtx@gmail.com>
2025-02-17 17:40:50 -0600

    tweaked

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include "main.h"
 #include "lemlib/api.hpp" //IWYU pragma: keep
+#include "lemlib/chassis/chassis.hpp"
+#include "lemlib/chassis/trackingWheel.hpp"
 
 /**
  * A callback function for LLEMU's center button.
@@ -16,10 +18,26 @@ void on_center_button() {
 		pros::lcd::clear_line(2);
 	}
 }
+//INTAKE = 12
+//COLOR SENSOR = 13
+//LADY BROWN = 1
+//LADY BROWN ROT. SENSOR = 11
+//MOGO MECH = H
+pros::MotorGroup left_motors({5, 2, 20});  // left motor ports
+pros::MotorGroup right_motors({-10, -9, -3}); // right motor ports
+pros::Imu imu(10);                              // imu port
 
-pros::MotorGroup left_motors({1, 2, 3});  // left motor ports
-pros::MotorGroup right_motors({4, 5, 6}); // right motor ports
-pros:: Imu imu(10);                             // imu port
+// tracking wheels
+// horizontal tracking wheel encoder. Rotation sensor, port 20, not reversed
+pros::Rotation horizontalEnc(19);
+// vertical tracking wheel encoder. Rotation sensor, port 11, reversed
+pros::Rotation verticalEnc0(6);
+pros::Rotation verticalEnc1(7);
+// horizontal tracking wheel. 2.75" diameter, 5.75" offset, back of the robot (negative)
+lemlib::TrackingWheel horizontal(&horizontalEnc, lemlib::Omniwheel::OLD_325, -5.75);
+// vertical tracking wheel. 2.75" diameter, 2.5" offset, left of the robot (negative)
+lemlib::TrackingWheel vertical0(&verticalEnc0, lemlib::Omniwheel::OLD_325, -2.5);
+lemlib::TrackingWheel vertical1(&verticalEnc0, lemlib::Omniwheel::OLD_325, -2.5);
 
 // drivetrain settings
 lemlib::Drivetrain drivetrain(&left_motors, // left motor group
@@ -30,16 +48,61 @@ lemlib::Drivetrain drivetrain(&left_motors, // left motor group
 							2 // horizontal drift is 2 (for now)
 							);
 
+// lateral motion controller
+lemlib::ControllerSettings linearController(10, // proportional gain (kP)
+                                            0, // integral gain (kI)
+                                            3, // derivative gain (kD)
+                                            3, // anti windup
+                                            1, // small error range, in inches
+                                            100, // small error range timeout, in milliseconds
+                                            3, // large error range, in inches
+                                            500, // large error range timeout, in milliseconds
+                                            20 // maximum acceleration (slew)
+);
+
+// angular motion controller
+lemlib::ControllerSettings angularController(2, // proportional gain (kP)
+                                             0, // integral gain (kI)
+                                             10, // derivative gain (kD)
+                                             3, // anti windup
+                                             1, // small error range, in degrees
+                                             100, // small error range timeout, in milliseconds
+                                             3, // large error range, in degrees
+                                             500, // large error range timeout, in milliseconds
+                                             0 // maximum acceleration (slew)
+);
+
+// sensors for odometry
+lemlib::OdomSensors sensors(&vertical0, // vertical tracking wheel
+	&vertical1, // vertical tracking wheel 2
+	&horizontal, // horizontal tracking wheel
+	nullptr, // horizontal tracking wheel 2 (DNE)
+	&imu // inertial sensor
+);
+
+// input curve for throttle input during driver control
+lemlib::ExpoDriveCurve throttleCurve(3, // joystick deadband out of 127
+	10, // minimum output where drivetrain will move out of 127
+	1.019 // expo curve gain
+);
+
+// input curve for steer input during driver control
+lemlib::ExpoDriveCurve steerCurve(3, // joystick deadband out of 127
+	10, // minimum output where drivetrain will move out of 127
+	1.019 // expo curve gain
+);
+
+lemlib::Chassis chassis(drivetrain, linearController, angularController, sensors, &throttleCurve, &steerCurve);
 /**
  * Runs initialization code. This occurs as soon as the program is started.
  *
  * All other competition modes are blocked by initialize; it is recommended
  * to keep execution time for this mode under a few seconds.
  */
-void initialize() {
+void initialize()
+{
 	pros::lcd::initialize();
 	pros::lcd::set_text(1, "Hello PROS User!");
-
 	pros::lcd::register_btn1_cb(on_center_button);
 }
 
@@ -48,7 +111,10 @@ void initialize() {
  * the VEX Competition Switch, following either autonomous or opcontrol. When
  * the robot is enabled, this task will exit.
  */
-void disabled() {}
+void disabled()
+{
+
+}
 
 /**
  * Runs after initialize(), and before autonomous when connected to the Field
@@ -59,7 +125,10 @@ void disabled() {}
  * This task will exit when the robot is enabled and autonomous or opcontrol
  * starts.
  */
-void competition_initialize() {}
+void competition_initialize()
+{
+
+}
 
 /**
  * Runs the user autonomous code. This function will be started in its own task
@@ -72,7 +141,10 @@ void competition_initialize() {}
  * will be stopped. Re-enabling the robot will restart the task, not re-start it
  * from where it left off.
  */
-void autonomous() {}
+void autonomous()
+{
+	
+}
 
 /**
  * Runs the operator control code. This function will be started in its own task
@@ -89,8 +161,8 @@ void autonomous() {}
  */
 void opcontrol() {
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::MotorGroup left_mg({1, -2, 3});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
-	pros::MotorGroup right_mg({-4, 5, -6});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
+	pros::MotorGroup left_mg({5, 2, 20});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
+	pros::MotorGroup right_mg({-10, -9, -3});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
 
 
 	while (true) {
@@ -103,6 +175,6 @@ void opcontrol() {
 		int turn = master.get_analog(ANALOG_RIGHT_X);  // Gets the turn left/right from right joystick
 		left_mg.move(dir - turn);                      // Sets left motor voltage
 		right_mg.move(dir + turn);                     // Sets right motor voltage
-		pros::delay(20);                               // Run for 20 ms then update
+		pros::delay(20);                          // Run for 20 ms then update
 	}
 }
