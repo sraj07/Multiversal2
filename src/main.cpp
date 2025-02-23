3b8f65dd9bdc2ec7750a9797937195619249d4ef
Sudev Raj <sudevrajtx@gmail.com>
2025-02-22 20:02:50 -0600

    updated ports

--- src/main.cpp
+++ src/main.cpp
@@ -166,7 +166,7 @@ void autonomous()
 void opcontrol() {
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
 	pros::MotorGroup left_mg({-4, -2, 20});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
-	pros::MotorGroup right_mg({-10, -3, 9});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
+	pros::MotorGroup right_mg({10, -3, 9});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
 
 	pros::Motor intake (-12);
 	pros::Motor ladybrown(1);
